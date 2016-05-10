c21e53f08f200325374b0469d1fb64c3b776d489
Dylan Sanders <Dylanjsanders1@gmail.com>
2016-05-10 15:17:26 -0700

    Finalized app.js, updated style.css and touched up index.html

--- app.js
+++ app.js
@@ -4,16 +4,45 @@ console.log('The user is named ' + userName);
 
 var firstResponse = prompt('Do I live with any animals?').toLowerCase();
 var firstAnswer = 'yes';
-if (firstAnswer === firstResponse || firstResponse[0] === firstAnswer[0]) {
-  alert('You are correct! ' + userName + ' I live with 1 cat.');
+console.log(firstResponse);
+if (firstAnswer === firstResponse || firstResponse === firstAnswer[0]) {
+  alert('You are correct! ' + userName + ' I live with one cat named Renly.');
 } else {
-  alert('Bummer, you are wrong ' + userName + ' better luck next time.');
+  alert('Bummer, you are wrong ' + userName + ' I live with a cat named Renly.');
 }
 
 var secondResponse = prompt('Do I live in Washington?').toLowerCase();
 var secondAnswer = 'yes';
-if (secondResponse === secondAnswer || secondResponse[0] === secondAnswer[0]) {
+console.log('The user answered ' + secondResponse);
+if (secondResponse === secondAnswer || secondResponse === secondAnswer[0]) {
   alert('You are correct ' + userName + ' I live in Seattle, Washington.');
 } else {
   alert('Wrong! You should have read the bio better for clues ' + userName + '.');
+}
+
+var thirdResponse = prompt('Is ice cream my favorite dessert?').toLowerCase();
+var thirdAnswer = 'no';
+console.log('The user answered ' + thirdResponse);
+if (thirdResponse === thirdAnswer || thirdResponse === thirdAnswer[0]) {
+  alert('Good guess, ' + userName + ' my favorite dessert is panna cotta.');
+} else {
+  alert('Nice try but ice cream is not my favorite dessert ' + userName + ', better luck next time.');
+}
+
+var fourthResponse = prompt('Have I been ski diving, bungee jumping and scuba diving?').toLowerCase();
+var fourthAnswer = 'no';
+console.log('The user answered ' + fourthResponse);
+if (fourthResponse === fourthAnswer || fourthResponse === fourthAnswer[0]) {
+  alert('You are correct ' + userName + ', I have been sky diving and bungee jumping but not scuba diving.');
+} else {
+  alert('Wrong! I have never been scuba diving.');
+}
+
+var fifthResponse = prompt('Do I love snowboarding?').toLowerCase();
+var fifthAnswer = 'yes';
+console.log('The user answered ' + fifthResponse);
+if (fifthResponse === fifthAnswer || fifthResponse === fifthAnswer[0]) {
+  alert('You are correct ' + userName + ', I do love snowboarding.');
+} else {
+  alert('You are wrong, ' + userName + ', I do love snowboarding');
 }
