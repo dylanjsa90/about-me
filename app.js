fb895008149ec3e9a4ba564d2fe5b328012e7f66
Dylan Sanders <Dylanjsanders1@gmail.com>
2016-05-11 16:14:58 -0700

    added 7th question to app.js, user input validation for all questions, touched up css and html

--- app.js
+++ app.js
@@ -7,12 +7,16 @@ var response;
 var myAnswers = ['yes', 'yes', 'no', 'no', 'yes'];
 var questions = ['Do I live with any animals?', 'Did I grow up in Washington?', 'Is ice cream my favorite dessert?',
 'Have I been ski diving, bungee jumping and scuba diving?', 'Do I love snowboarding?'];
-var myResponse = [', I live with a cat named Renly', ', I grew up on Bainbridge Island in Washington',
-', My favorite dessert is panna cotta', ', I have been bungee jumping and sky diving but not scuba diving', ', I do love snowboarding'];
+var myResponse = [', I live with a cat named Renly.', ', I grew up on Bainbridge Island in Washington.',
+', My favorite dessert is panna cotta.', ', I have been bungee jumping and sky diving but not scuba diving.', ', I do love snowboarding.'];
 
 for (var i = 0; i < questions.length; i++) {
   response = prompt(questions[i]).toLowerCase();
   console.log(response);
+  if (!isNaN(response)) {
+    alert('Please enter yes/y for yes or no/n for no');
+    response = prompt(questions[i].toLowerCase());
+  }
   if (response === myAnswers[i] || response === myAnswers[i][0]) {
     alert('Correct! ' + userName + myResponse[i]);
     correctAnswers += 1;
@@ -24,15 +28,50 @@ for (var i = 0; i < questions.length; i++) {
 var sixthAnswer = 26;
 for (var i = 3; i >= 0; i--) {
   var sixthResponse = parseInt(prompt('How old am I?'));
+  if (isNaN(sixthResponse)) {
+    alert('Not valid input please enter a numeric value.');
+    sixthResponse = parseInt(prompt('How old am I?'));
+  }
   if (sixthResponse === sixthAnswer) {
     alert('I am 26, Great guess!');
     correctAnswers += 1;
     i = -1;
-  }// else if (i = 0) {
-//  }
-  else if (sixthResponse > sixthAnswer) {
+  } else if (i === 0) {
+    alert('Sorry you ran out of guesses, I am 26 yours old');
+  } else if (sixthResponse > sixthAnswer) {
     alert('Wrong!, you guessed too high. You have ' + i + ' guesses remaining.');
   } else if (sixthResponse < sixthAnswer) {
-    alert('Wrong!, you guessed too low. You have ' + i + ' guesses remaining,');
+    alert('Wrong!, you guessed too low. You have ' + i + ' guesses remaining.');
   }
+}
+
+var endLoop = false;
+var seventhAnswers = ['ohio', ' california', ' illinois', ' montana'];
+var guessesRemaining = 6;
+
+while (endLoop === false || guessesRemaining === 0) {
+  var seventhResponse = prompt('What is another state I have lived besides Washington?').toLowerCase();
+  if (!isNaN(seventhResponse)) {
+    alert('Numeric values are not valid responses');
+    seventhResponse = prompt('What is another state I have lived besides Washington?').toLowerCase();
+  }
+  for (var i = 0; i < seventhAnswers.length; i++) {
+    if (seventhResponse === seventhAnswers[i]) {
+      endLoop = true;
+      correctAnswers += 1;
+      alert('Correct! I have lived in ' + seventhAnswers + '.');
+    }
+  }
+  if (endLoop === false) {
+    guessesRemaining -= 1;
+    alert('Wrong ' + userName + ' you have ' + guessesRemaining + ' guesses remaining.');
+  }
+}
+
+if (correctAnswers > 5) {
+  alert('Execellent Job ' + userName + ', you got ' + correctAnswers + ' out of 7 questions correct!');
+} else if (correctAnswers <= 5 && correctAnswers > 3) {
+  alert('Ehh... not bad ' + userName + ' you got ' + correctAnswers + ' out of 7 questions correct.');
+} else {
+  alert('Sorry ' + userName + ' you only got ' + correctAnswers + ' out of 7 questions correct, better luck next time.');
 }
