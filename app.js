a5fb6a71c343b2f2d3d03528ac364fb433213ca1
Dylan Sanders <Dylanjsanders1@gmail.com>
2016-05-12 15:58:28 -0700

    fixed question 7

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ var endLoop = false;
 var seventhAnswers = ['ohio', ' california', ' illinois', ' montana'];
 var guessesRemaining = 6;
 
-while (endLoop === false || guessesRemaining === 0) {
+while (endLoop === false || guessesRemaining !== 0) {
   var seventhResponse = prompt('What is another state I have lived besides Washington?').toLowerCase();
   if (!isNaN(seventhResponse)) {
     alert('Numeric values are not valid responses');
